9ad3e3ece93facb2694777a7d5c0afd6563d074b
Paul James <jamespaulc123@gmail.com>
2023-02-20 14:19:32 -0800

    Updated 2-print_alphabet.c task

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- *Main - This program prints lowercase a-z
+ *main - This program prints lowercase a-z
  *Return:Always 0 (Success)
  */
-int main(void)
+int main()
 {
 	char alpha = 'a';
 
@@ -14,6 +14,5 @@ int main(void)
 		alpha++;
 	}
 	putchar('\n');
-	
 	return (0);
 }
