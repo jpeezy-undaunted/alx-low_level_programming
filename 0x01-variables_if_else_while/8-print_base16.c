dadb29cf39f2f68aded3479d0bfcb4c0356aa4c4
Paul James <jamespaulc123@gmail.com>
2023-02-20 17:12:58 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,19 +9,16 @@ int main(void)
 {
 	int n = '0';
 	int a_to_f = 'a';
-	
 	while (n <= '9') /*print 0-9*/
 	{
 		putchar(n);
 		n++;
 	}
-	
 	while (a_to_f <= 'f') /*print a-f*/
 	{
 		putchar(a_to_f);
 		a_to_f++;
 	}
-	
 	putchar('\n');
 	return (0);
 }
