78bd24a25ce4bb624a8df6a81df827f8e47fb417
Paul James <jamespaulc123@gmail.com>
2023-02-20 17:48:02 -0800

    9-print_com b.c task

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ *main - This program prints 00 to 99
+ *Return:Always 0 (Success)
+ */
+
+int main(void)
+{
+	int tens;
+	int units;
+
+	for (tens = '0'; tens <= '9'; ten++) /*prints tens digits*/
+	{
+		for (units = '0'; units <= '9', units++) /*prints units digits*/
+		{
+			putchar(tens);
+			putchar(units);
+			if (!(tens == '9' && units == '9')) /*ignore comma at the end*/
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
