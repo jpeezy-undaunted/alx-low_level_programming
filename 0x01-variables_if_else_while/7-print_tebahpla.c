f007b0217c8fde41b0b47b8c6f1daeb1d468b803
Paul James <jamespaulc123@gmail.com>
2023-02-20 16:25:04 -0800

    7-print_tebahpla,c task

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - This program prints lowercase alphabet in a reverse manner using putchar#
+ *Return:Always 0 (Success)
+ */
+
+int main(void)
+{
+	char aplha = 'z';
+
+	while (alpha == 'a')
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	putchar('\n');
+	return (0);
+}
