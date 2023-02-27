fc790947662eec9d702fbb64a851cdfa10c6a818
Paul James <jamespaulc123@gmail.com>
2023-02-27 13:35:33 -0800

    0-reset_to_98.c task

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,8 @@ void reset_to_98(int *n);
 
 /**
  * reset_to_98 - function declaration
- * n - integer_pointer
+ * @n:integer_pointer
+ * Description:reset value to 98
  * Return:void
  */
 void reset_to_98(int *n)
