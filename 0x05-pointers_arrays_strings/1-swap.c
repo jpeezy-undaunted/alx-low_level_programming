3381cabe60152d87a04f3e38ea343364dc04785b
Paul James <jamespaulc123@gmail.com>
2023-02-27 14:04:50 -0800

    1-swap task

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -8,7 +8,7 @@ void swap_int(int *a, int *b);
  * @b:second integer
  * Return:void
  */
-void swap_int(int *a, int *b)
+void swap_int(int *a, int *b) /*This fuction swaps the argument values of the two integers*/
 {
 	int placeholder1 = *a;
 	int placeholder2 = *b;
