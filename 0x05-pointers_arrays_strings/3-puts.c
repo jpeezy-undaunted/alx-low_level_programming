0a67acb17758e71f3c7f0963580e0d0c55c0a1a7
Paul James <jamespaulc123@gmail.com>
2023-02-27 22:14:18 -0800

    3-puts .c task

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void _puts(char *str);
+/**
+ * _puts -  fuction declaration
+ * Description:prints a string
+ * @str:the string pointers
+ * Return:void
+ */
+void_puts(char *str)
+{
+	int index;
+
+	for (index = 0; *(str + index) != '\0'; index++)
+	{
+		_putchar(*(str + index));
+	}
+	_putchar('\n');
+}
