f43d33db272f6ca8fbe550707f25e1c035a325dd
Paul James <jamespaulc123@gmail.com>
2023-02-28 17:18:56 -0800

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
-void print_rev(char *s);
+#include "main.h"
+void print_rev(char *s)
 /**
  * print_rev - function declaration
  * @a: string pointer
  * Return:void
  */
-void print_rev(char *s)
 {
 	int index;
 	
