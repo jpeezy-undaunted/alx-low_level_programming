e202f779620c059bb6bde1a30c04b05c4e1c26d2
Paul James <jamespaulc123@gmail.com>
2023-02-28 18:47:21 -0800

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "2-strlen.c"
+
+void puts2(char *str);
+/**
+ * puts2 - function definition
+ * Description:'prints index divisible by 2'
+ * @str: string pointer
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int index;
+
+	for (index = 0; index < _strlen(str); index++)
+	{
+		if (index % 2 == 0)
+		{
+			putchar(str[index]);
+		}
+	}
+	_putchar('\n');
+}
