e90a1db83ef71dc657d16be234a7d9682e80d9f2
Paul James <jamespaulc123@gmail.com>
2023-03-02 17:17:06 -0800

    0-strcat.c task

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strncat - function that concatenatestwo strings
+ * using at most n bytes from src
+ * @dest:destination input value
+ * @src:source input value
+ * @n:input value pointer
+ * Return:dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j <n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
