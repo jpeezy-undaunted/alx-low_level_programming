795107d42ebac5b63efffa89b2cf6745aa8857a7
Paul James <jamespaulc123@gmail.com>
2023-03-02 20:46:19 -0800

    102-magic.c task

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+  /*
+   * main - function
+   * Description:prints 98 with a new line
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - you are to make only one statement
+   * - you are not allowed to code anything else than this line of code
+   * Return:0
+   */
+	int main(void);
+
+	(p + 5) = 98;
+  /* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
