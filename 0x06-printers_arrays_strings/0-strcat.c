9ac777e4f7877e4321c4ab6fed709679821eb787
Paul James <jamespaulc123@gmail.com>
2023-03-02 16:34:58 -0800

    0-strcat.c task

---
+++ 0x06-printers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strcat - function that concatenates two strings
+ * @dest:destination input value pointer
+ * @src:source input value pointer
+ * Return:void
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
