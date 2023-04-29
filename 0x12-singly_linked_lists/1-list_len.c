e8e27728f8d494016e1a1a5b578d547ebf7522af
Paul James <jamespaulc123@gmail.com>
2023-04-29 02:20:42 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,5 +1,5 @@
 #include "lists.h"
-#include <stddef>
+#include <stddef.h>
 /**
  * list_len - length of the string that returns number
  * of elemnents in the list
@@ -9,12 +9,12 @@
 
 size_t list_len(const list_h *h)
 {
-	size_t count = 0;
+	size_t node1 = 0;
 
 	while (h != NULL)
 	{
-		count++;
+		node1++;
 		h = h->next;
 	}
-	return (count);
+	return (node1);
 }
