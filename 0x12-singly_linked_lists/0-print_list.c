af0f1c813f91c6519df1ebb9993a93232db26814
Paul James <jamespaulc123@gmail.com>
2023-04-28 19:20:47 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,15 +9,13 @@
 
 size_t print_list(const list_t *h);
 {
-	const list_t *present = h;
-
 	size_t node1 = 0;
 
-	while (present != NULL)
+	while (h != NULL)
 	{
-		printf("[%d] %s\n", present->len, present->str != NULL ?
-				present->str : "(nil)");
-		present = present->next;
+		printf("[%d] %s\n", h->len, h->str != NULL ?
+				h->str : "(nil)");
+		h = h->next;
 		node1++;
 	}
 	return (node1);
