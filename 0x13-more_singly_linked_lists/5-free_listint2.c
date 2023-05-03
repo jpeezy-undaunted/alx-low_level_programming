41b8d357a1c2ca78af0310f7d8a94b219d0f188d
Paul James <jamespaulc123@gmail.com>
2023-05-02 22:07:50 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+/**
+ * free_listint2 - function frees a linked list
+ * @head: points to the listint_t list to be freed
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *end_node;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		end = (*head)->next;
+		free(*head);
+		*head = end_node;
+	}
+
+	*head = NULL;
+}
