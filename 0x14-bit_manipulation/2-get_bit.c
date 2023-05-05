fafa2f4dc12c6ff5760e5b8bda2d6d0378ece36d
Paul James <jamespaulc123@gmail.com>
2023-05-04 18:42:06 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,7 +10,7 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) * 8))
+	if (index >= (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
 
 	if ((n & (1 << index)) == 0)
